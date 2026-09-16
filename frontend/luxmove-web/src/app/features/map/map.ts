216429cab89736b7bc020bfcea0a49bd90d36422
cagrigoksu <cagrigoksu.ustundag@gmail.com>
2026-09-16 23:52:13 +0200

    feat: add Map

--- frontend/luxmove-web/src/app/features/map/map.ts
+++ frontend/luxmove-web/src/app/features/map/map.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-map',
@@ -6,4 +7,22 @@ import { Component } from '@angular/core';
   templateUrl: './map.html',
   styleUrl: './map.css',
 })
-export class Map {}
+export class Map implements AfterViewInit {
+  @ViewChild('map', { static: true })
+  mapElement!: ElementRef<HTMLDivElement>;
+
+  ngAfterViewInit(): void {
+    const map = L.map(this.mapElement.nativeElement).setView(
+      [49.6116, 6.1319],
+      12
+    );
+
+    setTimeout(() => {
+      map.invalidateSize();
+    }, 0);
+
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors',
+    }).addTo(map);
+  }
+}
